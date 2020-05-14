a3e54f0ed7ebcf72ce31a3dd1e11d277d6a6550b
Indibar <indibar@algo2trade.com>
2020-05-14 18:05:54 +0530

    After stock selection

--- Algo2TradeRobot/frmMainTabbed.vb
+++ Algo2TradeRobot/frmMainTabbed.vb
@@ -496,7 +496,7 @@ Public Class frmMainTabbed
         'End If
     End Function
     Private Async Sub btnNFOStart_Click(sender As Object, e As EventArgs) Handles btnNFOStart.Click
-        Dim authenticationUserId As String = "HYDJ39"
+        Dim authenticationUserId As String = "AB096403"
         If Common.GetAliceCredentialsFromSettings(_commonControllerUserInput).UserId.ToUpper IsNot Nothing AndAlso
             Common.GetAliceCredentialsFromSettings(_commonControllerUserInput).UserId.ToUpper <> "" AndAlso
             (authenticationUserId <> Common.GetAliceCredentialsFromSettings(_commonControllerUserInput).UserId.ToUpper AndAlso
