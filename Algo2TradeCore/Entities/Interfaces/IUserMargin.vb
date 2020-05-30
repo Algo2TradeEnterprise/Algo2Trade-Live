c8752797a3d74fbc81ef9ead65766a6fe4495934
Indibar <indibar@algo2trade.com>
2020-05-30 18:09:55 +0530

    After dll mode implementation

--- Algo2TradeCore/Entities/Interfaces/IUserMargin.vb
+++
@@ -1,7 +0,0 @@
-﻿Namespace Entities
-    Public Interface IUserMargin
-        ReadOnly Property Enabled As Boolean
-        ReadOnly Property NetAmount As Decimal
-        ReadOnly Property Broker As APISource
-    End Interface
-End Namespace
