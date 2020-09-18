7589125b5b06fd7b5aac0e9fd12298225b0ffa5a
Indibar <indibar@algo2trade.com>
2020-09-18 08:52:57 +0530

    After fix

--- Algo2TradeRobot/NFO/frmNFOTradableInstrumentList.vb
+++ Algo2TradeRobot/NFO/frmNFOTradableInstrumentList.vb
@@ -19,18 +19,18 @@
 
         If _TradableInstruments IsNot Nothing AndAlso _TradableInstruments.Count > 0 Then
             For Each instrument In _TradableInstruments
-                If instrument.StrategyInstrumentRunning Then
-                    Dim row As DataRow = dt.NewRow
-                    row("Instrument Name") = instrument.TradableInstrument.TradingSymbol
-                    row("Exchange") = instrument.TradableInstrument.RawExchange
-                    row("Instrument Type") = instrument.TradableInstrument.RawInstrumentType
-                    row("Expiry") = instrument.TradableInstrument.Expiry
-                    row("Lot Size") = instrument.TradableInstrument.LotSize
-                    row("Tick Size") = instrument.TradableInstrument.TickSize
-                    row("Historical") = instrument.TradableInstrument.IsHistoricalCompleted
-                    row("Running") = instrument.StrategyInstrumentRunning
-                    dt.Rows.Add(row)
-                End If
+                'If instrument.StrategyInstrumentRunning Then
+                Dim row As DataRow = dt.NewRow
+                row("Instrument Name") = instrument.TradableInstrument.TradingSymbol
+                row("Exchange") = instrument.TradableInstrument.RawExchange
+                row("Instrument Type") = instrument.TradableInstrument.RawInstrumentType
+                row("Expiry") = instrument.TradableInstrument.Expiry
+                row("Lot Size") = instrument.TradableInstrument.LotSize
+                row("Tick Size") = instrument.TradableInstrument.TickSize
+                row("Historical") = instrument.TradableInstrument.IsHistoricalCompleted
+                row("Running") = instrument.StrategyInstrumentRunning
+                dt.Rows.Add(row)
+                'End If
             Next
         End If
 
