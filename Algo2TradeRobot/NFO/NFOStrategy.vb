c34326018b3f6dab54373d0633070bb062effc7d
Indibar <indibar@algo2trade.com>
2020-10-07 16:28:16 +0530

    After fix

--- Algo2TradeRobot/NFO/NFOStrategy.vb
+++ Algo2TradeRobot/NFO/NFOStrategy.vb
@@ -151,7 +151,7 @@ Public Class NFOStrategy
                 If Me.GetTotalTurnover() <> 0 Then
                     Dim message As String = String.Format("PL:{0}, Max Capital: {1}, Timestamp: {2}",
                                                           Math.Round(Me.GetTotalPLAfterBrokerage(), 2),
-                                                          Math.Round(Me.GetTotalTurnover() / 10, 2),
+                                                          Math.Round(Me.MaxTurnover / 10, 2),
                                                           Now.ToString("HH:mm:ss"))
 
                     Await SendTelegramTextMessageAsync(Me.ParentController.UserInputs.TelegramAPIKey, "-412527350", message).ConfigureAwait(False)
